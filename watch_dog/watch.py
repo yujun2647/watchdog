e80ebdff808da4296595a20f1e1642224b0c275f
walkerjun <yujun2647@163.com>
2023-05-15 13:08:50 +0800

    fix(cli): prevent service startup before ArgumentParser

--- watch_dog/watch.py
+++ watch_dog/watch.py
@@ -1,5 +1,67 @@
 import os
 import argparse
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument(
+    "address", help="Camera address, like '/dev/video0' or "
+                    "'rtsp://[username]:[password]@[ip]'",
+    default="/dev/video0", type=str)
+
+parser.add_argument(
+    "-port", help="API server port, default: 8000",
+    default=8000, type=int)
+
+parser.add_argument(
+    "-width",
+    help="video width, like 1280",
+    default=1280,
+    type=int
+)
+parser.add_argument(
+    "-height",
+    help="video_height, like 720",
+    default=720,
+    type=int
+)
+
+parser.add_argument(
+    "-active-fps",
+    help="the fps when object detected, default active_fps is "
+         "os.cup_count() * 2",
+    default=os.cpu_count() * 2,
+    type=int
+)
+
+parser.add_argument(
+    "-rest-fps",
+    help="the fps when no object detected",
+    default=1,
+    type=int
+)
+
+parser.add_argument(
+    "-car-alart-secs",
+    help="car detected alart time",
+    default=2 * 60,
+    type=int
+)
+parser.add_argument(
+    "-cache-path",
+    help="the path store the videos",
+    default="/home/watch_dog/video_cache",
+    type=str
+)
+
+parser.add_argument(
+    "-cache-days",
+    help="the days cache videos",
+    default=30,
+    type=int
+)
+
+args = parser.parse_args()
+
 import logging
 
 from watch_dog.configs.constants import CameraConfig, PathConfig
@@ -16,66 +78,6 @@ from watch_dog.services.workshop import WorkShop
 
 
 def main():
-    parser = argparse.ArgumentParser()
-
-    parser.add_argument(
-        "address", help="Camera address, like '/dev/video0' or "
-                        "'rtsp://[username]:[password]@[ip]'",
-        default="/dev/video0", type=str)
-
-    parser.add_argument(
-        "-port", help="API server port, default: 8000",
-        default=8000, type=int)
-
-    parser.add_argument(
-        "-width",
-        help="video width, like 1280",
-        default=1280,
-        type=int
-    )
-    parser.add_argument(
-        "-height",
-        help="video_height, like 720",
-        default=720,
-        type=int
-    )
-
-    parser.add_argument(
-        "-active-fps",
-        help="the fps when object detected, default active_fps is "
-             "os.cup_count() * 2",
-        default=os.cpu_count() * 2,
-        type=int
-    )
-
-    parser.add_argument(
-        "-rest-fps",
-        help="the fps when no object detected",
-        default=1,
-        type=int
-    )
-
-    parser.add_argument(
-        "-car-alart-secs",
-        help="car detected alart time",
-        default=2 * 60,
-        type=int
-    )
-    parser.add_argument(
-        "-cache-path",
-        help="the path store the videos",
-        default="/home/watch_dog/video_cache",
-        type=str
-    )
-
-    parser.add_argument(
-        "-cache-days",
-        help="the days cache videos",
-        default=30,
-        type=int
-    )
-
-    args = parser.parse_args()
     camera_address = args.address
     PathConfig.CACHE_DATAS_PATH = args.cache_path
     CameraConfig.REST_FPS.value = args.rest_fps
