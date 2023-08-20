199a372a0a98c2ffcc7de434362c103debcd33dd
walkerjun <yujun2647@163.com>
2023-08-20 13:45:39 +0800

    feat(camera): active camera to high fps when there is camera request

--- watch_dog/services/workshop.py
+++ watch_dog/services/workshop.py
@@ -1,8 +1,7 @@
 import time
 from typing import *
-import logging
 from queue import Empty, Queue as TQueue
-from threading import Event as TEvent, Lock as TLock
+from threading import Event as TEvent
 
 from watch_dog.server.custom_server import EnhanceThreadedWSGIServer
 from watch_dog.configs.constants import CameraConfig
@@ -43,13 +42,6 @@ class WorkShop(object):
             fps=CameraConfig.REST_FPS.value, detect_worker_num=1,
             video_width=video_width,
             video_height=video_height)
-
-        self._stop_load_event = TEvent()
-        self.consume_req_event = TEvent()
-        self.consuming_req_event = TEvent()
-
-        self._live_frame_queues: List[TimeTQueue] = []
-        self._live_queues_lock = TLock()
 
         self.frame_dst = FrameDistributor(q_console=self.q_console)
 
@@ -94,20 +86,19 @@ class WorkShop(object):
             raise Empty
         return self._live_frame
 
-    def register_view_request(self) -> TimeTQueue:
-        with self._live_queues_lock:
-            ttq = TimeTQueue()
-            self._live_frame_queues.append(ttq)
-            return ttq
-
-    def notify_consume_req(self):
-        self.consume_req_event.set()
-
     @new_thread
     def preloading_live_frame2(self):
         render_frame_queue = self.q_console.render_frame_queue
 
+        camera_active = False
         while True:
+            if not camera_active and self.q_console.cam_viewing():
+                self.q_console.active_camera(tag="view request")
+                camera_active = True
+            elif camera_active and not self.q_console.cam_viewing():
+                self.q_console.rest_camera(tag="view request end")
+                camera_active = False
+
             frame_box: FrameBox = render_frame_queue.get()
             frame_box.put_delay_text("final")
             frame_box.next_come = TEvent()
@@ -118,42 +109,6 @@ class WorkShop(object):
             self._live_frame.next = frame_box
             self._live_frame.next_come.set()
             self._live_frame = frame_box
-
-    @new_thread
-    def preloading_live_frame(self):
-        render_frame_queue = self.q_console.render_frame_queue
-
-        while True:
-            if self._stop_load_event.is_set():
-                return
-            if not self.consume_req_event.is_set():
-                # print("no consume req...")
-                time.sleep(0.5)
-                continue
-
-            if not self.consuming_req_event.wait(timeout=5):
-                logging.info("No consuming req event, exit preload")
-                self.consume_req_event.clear()
-                self._live_frame_queues.clear()
-                continue
-            try:
-                frame_box: FrameBox = render_frame_queue.get(timeout=5)
-                frame_box.put_delay_text(tag="final")
-                with self._live_queues_lock:
-                    expires = []
-                    for queue in self._live_frame_queues:
-                        if queue.break_time > 30:
-                            expires.append(queue)
-                            continue
-                        queue.put(frame_box)
-                    for queue in expires:
-                        self._live_frame_queues.remove(queue)
-                        logging.info(f"queue: {queue.break_time} expired, "
-                                     f"removed")
-
-                self.q_console.live_frame = frame_box
-            except Empty:
-                continue
 
     def test_monitor(self):
         from cv2 import cv2
